7c4a5b9638d8484f25233d61de93bd264be4e04c
Gregory Green <green_gregory@yahoo.com>
2025-01-30 21:25:01 -0500

    adding sql consumer sink

---
+++ src/main/java/showcase/ai/data/orchestration/scdf/properties/SqlConsumerProperties.java
@@ -0,0 +1,25 @@
+package showcase.ai.data.orchestration.scdf.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ConfigurationProperties(prefix = "sql.consumer")
+public class SqlConsumerProperties {
+
+    /**
+     * Sql to execute.
+     *
+     * Note the Json payload is passed as SQL parameters
+     * based on field names.
+     *
+     * Ex: insert into customers(ID,NAME) values(:id,:firstName)
+     */
+    private String sql;
+}
