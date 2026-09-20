879a036c1451d19add1d4dfe073507d089c1fb3d
ggreen <gregoryg@vmware.com>
2026-09-20 09:49:04 -0400

    adding mapping

---
+++ applications/sinks/iceberg-s3-sink/README.md
@@ -0,0 +1,9 @@
+
+
+```json
+{
+  "id" : 1,
+  "name" : "Josiah Imani",
+  "email" : "jImani@cloudNativeData.io"
+}
+```
--- applications/sinks/iceberg-s3-sink/src/main/java/io/cloudNativeData/iceberg/functions/IceBergConsumer.java
+++ applications/sinks/iceberg-s3-sink/src/main/java/io/cloudNativeData/iceberg/functions/IceBergConsumer.java
@@ -2,6 +2,7 @@ package io.cloudNativeData.iceberg.functions;
 
 import io.cloudNativeData.iceberg.repository.IcebergTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,12 +10,15 @@ import java.util.function.Consumer;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class IceBergConsumer implements Consumer<Map<String, Object>> {
 
     private final IcebergTemplate icebergTemplate;
 
     @Override
-    public void accept(Map<String, Object> stringObjectMap) {
-        icebergTemplate.writeRecord(stringObjectMap);
+    public void accept(Map<String, Object> map) {
+
+        log.info("Iceberg consumer received data: {}", map);
+        icebergTemplate.writeRecord(map);
     }
 }
